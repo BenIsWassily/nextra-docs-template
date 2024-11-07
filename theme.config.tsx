84be26fffcb95235ea25d0c3e98dc9bfbe623d94
BenIsWassily <benoitcm@gmail.com>
2024-11-08 00:24:01 +0100

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>On s'en fou</span>,
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
   },
